e8c932c8886291c6c2ace6fcdcab9fb645b226f9
Nico <juareznico61@gmail.com>
2023-08-15 17:17:05 -0300

    cambios user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -45,6 +45,7 @@ export const login = async (req,res) => {
 
         res.cookie("token", token)
         res.json({
+            message: "Conectado",
             id: userFound._id,
             username: userFound.username,
             email: userFound.email
@@ -54,3 +55,10 @@ export const login = async (req,res) => {
         res.status(500).json({ message: error.message })
     }
 };
+
+export const logout = async (req, res) => {
+    res.cookie("token", "", {
+      expires: new Date(0),
+    });
+    return res.status(200).json({message:"Usuario desconectado"});
+  };
