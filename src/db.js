ca8257623a05720f2003a58821b71731c4d06a60
Nico <juareznico61@gmail.com>
2023-08-23 18:44:32 -0300

    register listo

--- src/db.js
+++ src/db.js
@@ -4,12 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const connectDB = async () => {
-    
+
     try {
         const dbUrl = process.env.DB_URL;
         await mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("Base de datos conectada");
-    } catch (error) {
+    } 
+    catch (error) {
         console.error("Error al conectar a la base de datos:", error);
     }
 };
